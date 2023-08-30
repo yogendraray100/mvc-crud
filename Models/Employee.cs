754ff213c9a9a31202383ac739ab0e70318d3435
yogendraray100 <yogendraray100@gmail.com>
2023-08-30 16:37:22 +0545

    first commit

---
+++ Models/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Asp.netCoreMvcCrud.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+
+        [Column(TypeName = "nvarchar(250)")]
+        [Required(ErrorMessage = "This field is required")]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [Column(TypeName = "nvarchar(250)")]
+        [Required(ErrorMessage = "This field is required")]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [Column(TypeName = "varchar(10)")]
+        [DisplayName("Employee Code")]
+        public string EmpCode { get; set; }
+
+		[Column(TypeName = "varchar(100)")]
+		public string Department { get; set; }
+
+		[Column(TypeName = "varchar(100)")]
+        public string Position { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        public string Email { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        [DisplayName("Office Location")]
+        public string OfficeLocation { get; set; }
+    }
+}
